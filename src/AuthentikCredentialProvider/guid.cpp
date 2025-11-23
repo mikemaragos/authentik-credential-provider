f4023324c6d5c15463fa9bc56d55d0af2a95f3b3
mikemaragos <82425851+mikemaragos@users.noreply.github.com>
2025-11-22 23:10:54 -0600

    Simplify GUID definition - use DEFINE_GUID directly with initguid.h

--- src/AuthentikCredentialProvider/guid.cpp
+++ src/AuthentikCredentialProvider/guid.cpp
@@ -2,11 +2,9 @@
 // GUID definition for the credential provider
 
 #include <windows.h>
+#include <initguid.h>
 
-// Define INITGUID before including guid.h to actually define the GUID
-#define INITGUID
-#include <guiddef.h>
-#include "guid.h"
-
-// The GUID is defined here because INITGUID is defined
-// This creates the actual storage for CLSID_AuthentikCredentialProvider
+// {8B7C4F9E-2A3D-4E5F-9C1B-7D8E6F4A5B3C}
+// Explicitly define the GUID here
+DEFINE_GUID(CLSID_AuthentikCredentialProvider,
+    0x8b7c4f9e, 0x2a3d, 0x4e5f, 0x9c, 0x1b, 0x7d, 0x8e, 0x6f, 0x4a, 0x5b, 0x3c);
