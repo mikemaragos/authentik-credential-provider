23d5a9ef7675bd7bef8bcc79807ebce6325a252d
mikemaragos <82425851+mikemaragos@users.noreply.github.com>
2025-11-22 23:07:12 -0600

    Add guid.cpp to define CLSID

---
+++ src/AuthentikCredentialProvider/guid.cpp
@@ -0,0 +1,9 @@
+// guid.cpp
+// GUID definition for the credential provider
+
+#include <windows.h>
+#include <initguid.h>
+#include "guid.h"
+
+// The GUID is defined here (not just declared)
+// This creates the actual storage for the GUID
