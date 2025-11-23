90f7f937f343b352855cae53b036e13d169a6cfa
mikemaragos <82425851+mikemaragos@users.noreply.github.com>
2025-11-22 23:07:13 -0600

    Update guid.h to use extern declaration

--- src/AuthentikCredentialProvider/guid.h
+++ src/AuthentikCredentialProvider/guid.h
@@ -7,8 +7,14 @@
 
 // {8B7C4F9E-2A3D-4E5F-9C1B-7D8E6F4A5B3C}
 // Generate your own GUID using guidgen.exe or Visual Studio
+
+// Declare the GUID as extern (defined in guid.cpp)
+#ifdef INITGUID
 DEFINE_GUID(CLSID_AuthentikCredentialProvider,
     0x8b7c4f9e, 0x2a3d, 0x4e5f, 0x9c, 0x1b, 0x7d, 0x8e, 0x6f, 0x4a, 0x5b, 0x3c);
+#else
+extern const GUID CLSID_AuthentikCredentialProvider;
+#endif
 
 // DLL reference counting
 void DllAddRef();
