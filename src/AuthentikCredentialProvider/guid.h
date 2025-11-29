9b1b1748dd9e40a0f211a350434e3c34e6c7986b
mikemaragos <82425851+mikemaragos@users.noreply.github.com>
2025-11-28 19:51:25 -0600

    Fix: guid.h

--- src/AuthentikCredentialProvider/guid.h
+++ src/AuthentikCredentialProvider/guid.h
@@ -6,13 +6,9 @@
 #include <guiddef.h>
 
 // {A1B2C3D4-E5F6-4A5B-9C8D-7E6F5A4B3C2D}
-// Authentik Passwordless Credential Provider CLSID
-DEFINE_GUID(CLSID_AuthentikPasswordlessCP,
+DEFINE_GUID(CLSID_AuthentikCredentialProvider,
     0xa1b2c3d4, 0xe5f6, 0x4a5b, 0x9c, 0x8d, 0x7e, 0x6f, 0x5a, 0x4b, 0x3c, 0x2d);
 
 // DLL reference counting
 void DllAddRef();
 void DllRelease();
-
-// Global DLL instance
-extern HINSTANCE g_hinst;
