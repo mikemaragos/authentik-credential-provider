16daa398f45f7cdcaf3bace4c3092b22905e06b7
mikemaragos <82425851+mikemaragos@users.noreply.github.com>
2025-11-29 13:28:56 -0600

    Fix AuthentikKSPDll.cpp - define our own function table structure to avoid SDK issues

--- src/AuthentikKSP/AuthentikKSPDll.cpp
+++ src/AuthentikKSP/AuthentikKSPDll.cpp
@@ -2,6 +2,101 @@
 // DLL entry point and NCrypt function table export
 
 #include "AuthentikKSP.h"
+#include <ntstatus.h>
+
+// ============================================================================
+// NCrypt Key Storage Provider Function Table Definition
+// 
+// This structure must match what NCrypt expects. We define it ourselves
+// because the SDK headers can be inconsistent.
+// ============================================================================
+
+typedef SECURITY_STATUS (WINAPI *KspOpenProviderFn)(
+    NCRYPT_PROV_HANDLE*, LPCWSTR, DWORD);
+typedef SECURITY_STATUS (WINAPI *KspOpenKeyFn)(
+    NCRYPT_PROV_HANDLE, NCRYPT_KEY_HANDLE*, LPCWSTR, DWORD, DWORD);
+typedef SECURITY_STATUS (WINAPI *KspCreatePersistedKeyFn)(
+    NCRYPT_PROV_HANDLE, NCRYPT_KEY_HANDLE*, LPCWSTR, LPCWSTR, DWORD, DWORD);
+typedef SECURITY_STATUS (WINAPI *KspGetProviderPropertyFn)(
+    NCRYPT_PROV_HANDLE, LPCWSTR, PBYTE, DWORD, DWORD*, DWORD);
+typedef SECURITY_STATUS (WINAPI *KspGetKeyPropertyFn)(
+    NCRYPT_PROV_HANDLE, NCRYPT_KEY_HANDLE, LPCWSTR, PBYTE, DWORD, DWORD*, DWORD);
+typedef SECURITY_STATUS (WINAPI *KspSetProviderPropertyFn)(
+    NCRYPT_PROV_HANDLE, LPCWSTR, PBYTE, DWORD, DWORD);
+typedef SECURITY_STATUS (WINAPI *KspSetKeyPropertyFn)(
+    NCRYPT_PROV_HANDLE, NCRYPT_KEY_HANDLE, LPCWSTR, PBYTE, DWORD, DWORD);
+typedef SECURITY_STATUS (WINAPI *KspFinalizeKeyFn)(
+    NCRYPT_PROV_HANDLE, NCRYPT_KEY_HANDLE, DWORD);
+typedef SECURITY_STATUS (WINAPI *KspDeleteKeyFn)(
+    NCRYPT_PROV_HANDLE, NCRYPT_KEY_HANDLE, DWORD);
+typedef SECURITY_STATUS (WINAPI *KspFreeProviderFn)(
+    NCRYPT_PROV_HANDLE);
+typedef SECURITY_STATUS (WINAPI *KspFreeKeyFn)(
+    NCRYPT_PROV_HANDLE, NCRYPT_KEY_HANDLE);
+typedef SECURITY_STATUS (WINAPI *KspFreeBufferFn)(
+    PVOID);
+typedef SECURITY_STATUS (WINAPI *KspEncryptFn)(
+    NCRYPT_PROV_HANDLE, NCRYPT_KEY_HANDLE, PBYTE, DWORD, VOID*, PBYTE, DWORD, DWORD*, DWORD);
+typedef SECURITY_STATUS (WINAPI *KspDecryptFn)(
+    NCRYPT_PROV_HANDLE, NCRYPT_KEY_HANDLE, PBYTE, DWORD, VOID*, PBYTE, DWORD, DWORD*, DWORD);
+typedef SECURITY_STATUS (WINAPI *KspIsAlgSupportedFn)(
+    NCRYPT_PROV_HANDLE, LPCWSTR, DWORD);
+typedef SECURITY_STATUS (WINAPI *KspEnumAlgorithmsFn)(
+    NCRYPT_PROV_HANDLE, DWORD, DWORD*, NCryptAlgorithmName**, DWORD);
+typedef SECURITY_STATUS (WINAPI *KspEnumKeysFn)(
+    NCRYPT_PROV_HANDLE, LPCWSTR, NCryptKeyName**, PVOID*, DWORD);
+typedef SECURITY_STATUS (WINAPI *KspImportKeyFn)(
+    NCRYPT_PROV_HANDLE, NCRYPT_KEY_HANDLE, LPCWSTR, NCryptBufferDesc*, NCRYPT_KEY_HANDLE*, PBYTE, DWORD, DWORD);
+typedef SECURITY_STATUS (WINAPI *KspExportKeyFn)(
+    NCRYPT_PROV_HANDLE, NCRYPT_KEY_HANDLE, NCRYPT_KEY_HANDLE, LPCWSTR, NCryptBufferDesc*, PBYTE, DWORD, DWORD*, DWORD);
+typedef SECURITY_STATUS (WINAPI *KspSignHashFn)(
+    NCRYPT_PROV_HANDLE, NCRYPT_KEY_HANDLE, VOID*, PBYTE, DWORD, PBYTE, DWORD, DWORD*, DWORD);
+typedef SECURITY_STATUS (WINAPI *KspVerifySignatureFn)(
+    NCRYPT_PROV_HANDLE, NCRYPT_KEY_HANDLE, VOID*, PBYTE, DWORD, PBYTE, DWORD, DWORD);
+typedef SECURITY_STATUS (WINAPI *KspPromptUserFn)(
+    NCRYPT_PROV_HANDLE, NCRYPT_KEY_HANDLE, LPCWSTR, DWORD);
+typedef SECURITY_STATUS (WINAPI *KspNotifyChangeKeyFn)(
+    NCRYPT_PROV_HANDLE, HANDLE*, DWORD);
+typedef SECURITY_STATUS (WINAPI *KspSecretAgreementFn)(
+    NCRYPT_PROV_HANDLE, NCRYPT_KEY_HANDLE, NCRYPT_KEY_HANDLE, NCRYPT_SECRET_HANDLE*, DWORD);
+typedef SECURITY_STATUS (WINAPI *KspDeriveKeyFn)(
+    NCRYPT_PROV_HANDLE, NCRYPT_SECRET_HANDLE, LPCWSTR, NCryptBufferDesc*, PUCHAR, DWORD, DWORD*, ULONG);
+typedef SECURITY_STATUS (WINAPI *KspFreeSecretFn)(
+    NCRYPT_PROV_HANDLE, NCRYPT_SECRET_HANDLE);
+
+// KSP Interface Version
+#define NCRYPT_KEY_STORAGE_INTERFACE_VERSION_1  0x00010001
+
+// Our function table structure
+typedef struct _AUTHENTIK_KSP_FUNCTION_TABLE {
+    DWORD                       Version;
+    KspOpenProviderFn           OpenProvider;
+    KspOpenKeyFn                OpenKey;
+    KspCreatePersistedKeyFn     CreatePersistedKey;
+    KspGetProviderPropertyFn    GetProviderProperty;
+    KspGetKeyPropertyFn         GetKeyProperty;
+    KspSetProviderPropertyFn    SetProviderProperty;
+    KspSetKeyPropertyFn         SetKeyProperty;
+    KspFinalizeKeyFn            FinalizeKey;
+    KspDeleteKeyFn              DeleteKey;
+    KspFreeProviderFn           FreeProvider;
+    KspFreeKeyFn                FreeKey;
+    KspFreeBufferFn             FreeBuffer;
+    KspEncryptFn                Encrypt;
+    KspDecryptFn                Decrypt;
+    KspIsAlgSupportedFn         IsAlgSupported;
+    KspEnumAlgorithmsFn         EnumAlgorithms;
+    KspEnumKeysFn               EnumKeys;
+    KspImportKeyFn              ImportKey;
+    KspExportKeyFn              ExportKey;
+    KspSignHashFn               SignHash;
+    KspVerifySignatureFn        VerifySignature;
+    KspPromptUserFn             PromptUser;
+    KspNotifyChangeKeyFn        NotifyChangeKey;
+    KspSecretAgreementFn        SecretAgreement;
+    KspDeriveKeyFn              DeriveKey;
+    KspFreeSecretFn             FreeSecret;
+} AUTHENTIK_KSP_FUNCTION_TABLE;
 
 // ============================================================================
 // DLL Entry Point
@@ -32,8 +127,8 @@ BOOL APIENTRY DllMain(
 // ============================================================================
 
 // The function table that NCrypt will use to call our provider
-static NCRYPT_KEY_STORAGE_FUNCTION_TABLE g_FunctionTable = {
-    NCRYPT_KEY_STORAGE_INTERFACE_VERSION,       // Version
+static AUTHENTIK_KSP_FUNCTION_TABLE g_FunctionTable = {
+    NCRYPT_KEY_STORAGE_INTERFACE_VERSION_1,     // Version
 
     // Core functions
     AuthentikKSPOpenProvider,                   // OpenProvider
@@ -74,24 +169,13 @@ static NCRYPT_KEY_STORAGE_FUNCTION_TABLE g_FunctionTable = {
 extern "C" __declspec(dllexport)
 NTSTATUS WINAPI GetKeyStorageInterface(
     _In_ LPCWSTR pszProviderName,
-    _Out_ NCRYPT_KEY_STORAGE_FUNCTION_TABLE** ppFunctionTable,
+    _Out_ PVOID* ppFunctionTable,
     _In_ DWORD dwFlags)
 {
+    if (ppFunctionTable == NULL)
+        return STATUS_INVALID_PARAMETER;
+
     // Return our function table
     *ppFunctionTable = &g_FunctionTable;
-    return ERROR_SUCCESS;
-}
-
-// Alternative entry point name that some versions of Windows use
-extern "C" __declspec(dllexport)
-NTSTATUS WINAPI GetKeyStorageInterfaceEx(
-    _In_ LPCWSTR pszProviderName,
-    _In_ DWORD dwFlags,
-    _Out_ NCRYPT_KEY_STORAGE_FUNCTION_TABLE** ppFunctionTable,
-    _Out_ DWORD* pdwSize)
-{
-    *ppFunctionTable = &g_FunctionTable;
-    if (pdwSize)
-        *pdwSize = sizeof(NCRYPT_KEY_STORAGE_FUNCTION_TABLE);
-    return ERROR_SUCCESS;
+    return STATUS_SUCCESS;
 }
