a2c04f8503df3e44d262470826c0cf42eb0b4390
mikemaragos <82425851+mikemaragos@users.noreply.github.com>
2025-11-28 20:04:39 -0600

    Add guid.cpp to define CLSID

---
+++ src/AuthentikCredentialProvider/guid.cpp
@@ -0,0 +1,7 @@
+// guid.cpp
+// GUID definition for the Authentik Passwordless Credential Provider
+// This file instantiates the GUID declared in guid.h
+
+#include <windows.h>
+#include <initguid.h>  // Must be included before guid.h to actually define the GUID
+#include "guid.h"
