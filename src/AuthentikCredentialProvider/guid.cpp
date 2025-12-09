97390bf6f6e2766df5c045e8295c03f5254de768
mikemaragos <82425851+mikemaragos@users.noreply.github.com>
2025-12-08 20:46:06 -0600

    Cleanup: Remove old file

--- src/AuthentikCredentialProvider/guid.cpp
+++
@@ -1,7 +0,0 @@
-// guid.cpp
-// GUID definition for the Authentik Passwordless Credential Provider
-// This file instantiates the GUID declared in guid.h
-
-#include <windows.h>
-#include <initguid.h>  // Must be included before guid.h to actually define the GUID
-#include "guid.h"
